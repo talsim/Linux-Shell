0478967702ee7ebe754ce2657de9335121799120
Tal <talsimhayev@gmail.com>
2020-04-02 11:14:23 +0000

    working on getting user input

--- src/myshell.c
+++ src/myshell.c
@@ -3,15 +3,26 @@
 #include "../include/LineParser.h"
 #include "../include/myshell.h"
 
+#define MAX_BUFFER_SIZE 2048
+
 int main()
 {
     printDirectory();
+
+    char buffer[MAX_BUFFER_SIZE] = "";
+    while (1)
+    {
+        if (fgets(buffer, MAX_BUFFER_SIZE, stdin) != NULL)
+        {
+            printf("user input: %s", buffer);
+        }
+    }
     return 1;
 }
 
 void printDirectory()
 {
-    char cwd[100];
+    char cwd[100] = "";
     if (getcwd(cwd, sizeof(cwd)) != NULL)
         printf("> Current working direcotry: %s\n", cwd);
 }
